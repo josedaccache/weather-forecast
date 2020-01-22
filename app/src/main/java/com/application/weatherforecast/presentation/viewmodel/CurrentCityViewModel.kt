4ea47c8f384aebe82e3c6f65f4ababb07e9f9283
jose daccache <jose.d@euriskomobility.com>
2020-01-22 10:58:32 +0200

    feat: add unit test

--- app/src/main/java/com/application/weatherforecast/presentation/viewmodel/CurrentCityViewModel.kt
+++ app/src/main/java/com/application/weatherforecast/presentation/viewmodel/CurrentCityViewModel.kt
@@ -1,6 +1,5 @@
 package com.application.weatherforecast.presentation.viewmodel
 
-import android.text.TextUtils
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.application.weatherforecast.di.DaggerApiComponent
--- app/src/test/java/com/application/weatherforecast/ForecastRemoteSourceTest.kt
+++ app/src/test/java/com/application/weatherforecast/ForecastRemoteSourceTest.kt
@@ -1,15 +1,18 @@
 package com.application.weatherforecast
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import com.application.weatherforecast.domain.CurrentCityUseCase
+import com.application.weatherforecast.data.remote.service.ForecastService
+import com.application.weatherforecast.data.remote.source.ForecastRemoteSource
+import com.application.weatherforecast.domain.CityUseCase
 import com.application.weatherforecast.model.*
-import com.application.weatherforecast.presentation.viewmodel.CurrentCityViewModel
+import com.application.weatherforecast.presentation.viewmodel.CityViewModel
+import com.application.weatherforecast.utils.APP_KEY
+import com.application.weatherforecast.utils.METRIC
 import io.reactivex.Scheduler
 import io.reactivex.Single
 import io.reactivex.android.plugins.RxAndroidPlugins
 import io.reactivex.disposables.Disposable
 import io.reactivex.internal.schedulers.ExecutorScheduler
-import io.reactivex.observers.TestObserver
 import io.reactivex.plugins.RxJavaPlugins
 import org.junit.Assert
 import org.junit.Before
@@ -22,23 +25,49 @@ import org.mockito.MockitoAnnotations
 import java.util.concurrent.Executor
 import java.util.concurrent.TimeUnit
 
-class CurrentCityViewModelTest {
+class ForecastRemoteSourceTest {
     @get:Rule
     var rule = InstantTaskExecutorRule()
 
     @Mock
-    lateinit var currentCityUseCase: CurrentCityUseCase
+    lateinit var service: ForecastService
 
     @InjectMocks
-    var currentCityViewModel = CurrentCityViewModel()
+    var forecastRemoteSource = ForecastRemoteSource()
 
-    private var testSingle: Single<FiveDays>? = null
+    private var testSingleForecast: Single<Forecast>? = null
+
+    private var testSingleFiveDays: Single<FiveDays>? = null
 
     @Before
     fun setup() {
         MockitoAnnotations.initMocks(this)
     }
 
+
+    @Test
+    fun getCityForecastSuccess() {
+        val main = Main(3.0, 1.0, 5.0)
+        val wind = Wind(35.0, "deg")
+        val weather = Weather("id", "main", "description", "icon")
+        val weatherList = arrayListOf(weather)
+        val forecast = Forecast(1L, main, wind, weatherList, "name")
+
+        testSingleForecast = Single.just(forecast)
+        Mockito.`when`(service.getForecast(APP_KEY, METRIC,"cityName")).thenReturn(testSingleForecast)
+        val response = forecastRemoteSource.getForecast("cityName")
+
+        Assert.assertEquals(testSingleForecast, response)
+
+    }
+
+    @Test
+    fun getCityForecastFailure() {
+        testSingleForecast = Single.error(Throwable())
+        Mockito.`when`(service.getForecast(APP_KEY, METRIC,"cityName")).thenReturn(testSingleForecast)
+        val response = forecastRemoteSource.getForecast("cityName")
+        Assert.assertEquals(testSingleForecast, response)
+    }
 
     @Test
     fun getCurrentCityForecastSuccess() {
@@ -58,36 +87,20 @@ class CurrentCityViewModelTest {
         val city = City("id","name", "country")
         val fiveDays = FiveDays(forecastList, city)
 
-        testSingle = Single.just(fiveDays)
-        Mockito.`when`(currentCityUseCase.getForecast("lat","long")).thenReturn(testSingle)
-        currentCityViewModel.fetchForecast("lat","long")
+        testSingleFiveDays = Single.just(fiveDays)
+        Mockito.`when`(service.getForecastByLocation(APP_KEY, METRIC,"lat","long")).thenReturn(testSingleFiveDays)
+        val response = forecastRemoteSource.getForecastByLocation("lat","long")
 
-        Assert.assertEquals(1, currentCityViewModel.forecast.value?.size)
-        Assert.assertEquals(false, currentCityViewModel.loading.value)
-        Assert.assertEquals(false, currentCityViewModel.forecastLoadError.value)
-        //Test Response Data
-        Assert.assertEquals(city.name + ", " + city.country, currentCityViewModel.cityName.value)
-        Assert.assertEquals(main.temp, currentCityViewModel.forecast.value?.get(0)?.list?.get(0)?.main?.temp)
-        Assert.assertEquals(main.tempMin, currentCityViewModel.forecast.value?.get(0)?.list?.get(0)?.main?.tempMin)
-        Assert.assertEquals(main.tempMax, currentCityViewModel.forecast.value?.get(0)?.list?.get(0)?.main?.tempMax)
-        Assert.assertEquals(wind.speed, currentCityViewModel.forecast.value?.get(0)?.list?.get(0)?.wind?.speed)
-        Assert.assertEquals(
-            weather.description,
-            currentCityViewModel.forecast.value?.get(0)?.list?.get(0)?.weather?.get(0)?.description
-        )
+        Assert.assertEquals(testSingleFiveDays, response)
 
     }
 
     @Test
     fun getCurrentCityForecastFailure() {
-        testSingle = Single.error(Throwable())
-        Mockito.`when`(currentCityUseCase.getForecast("lat", "long")).thenReturn(testSingle)
-
-        currentCityViewModel.fetchForecast("lat", "long")
-
-        Assert.assertEquals(null, currentCityViewModel.forecast.value?.size)
-        Assert.assertEquals(false, currentCityViewModel.loading.value)
-        Assert.assertEquals(true, currentCityViewModel.forecastLoadError.value)
+        testSingleFiveDays = Single.error(Throwable())
+        Mockito.`when`(service.getForecastByLocation(APP_KEY, METRIC,"lat","long")).thenReturn(testSingleFiveDays)
+        val response = forecastRemoteSource.getForecastByLocation("lat","long")
+        Assert.assertEquals(testSingleFiveDays, response)
     }
 
 
@@ -102,6 +115,7 @@ class CurrentCityViewModelTest {
                 return ExecutorScheduler.ExecutorWorker(Executor { it.run() })
             }
         }
+        RxJavaPlugins.reset()
         RxJavaPlugins.setInitIoSchedulerHandler { scheduler -> immediate }
         RxJavaPlugins.setInitComputationSchedulerHandler { scheduler -> immediate }
         RxJavaPlugins.setInitNewThreadSchedulerHandler { scheduler -> immediate }
